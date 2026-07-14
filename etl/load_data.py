7b9cf23e81ef09dcd4ff9feab469e6692595d6d8
irlhasnain <hasnainkh3011@gmail.com>
2026-07-14 22:24:19 +0530

    Add customer and loading functions

--- etl/load_data.py
+++ etl/load_data.py
@@ -12,3 +12,13 @@ def load_product(df,conn):
     product_df = df[['product_id', 'product_name', 'category', 'sub_category']].drop_duplicates()
     product_df.to_sql('product', conn, if_exists='replace', index=False)
     print(f"Loaded {len(product_df)} products")
+
+def load_order(df, conn):
+    order_df = df[['order_id', 'customer_id', 'order_date', 'ship_date']].drop_duplicates()
+    order_df.to_sql('order', conn, if_exists='replace', index=False)
+    print(f"Loaded {len(order_df)} orders")
+
+def load_order_item(df, conn):
+    order_item_df = df[['order_id', 'product_id', 'quantity', 'sales', 'profit']]
+    order_item_df.to_sql('order_item', conn, if_exists='replace', index=False)
+    print(f"Loaded {len(order_item_df)} order items")
