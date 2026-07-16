511f58fe702186bd31b5513cca7f4283fd8b8a3d
irlhasnain <hasnainkh3011@gmail.com>
2026-07-17 00:15:49 +0530

    Add ETL pipeline to load cleaned data into SQLite database

--- etl/load_data.py
+++ etl/load_data.py
@@ -6,7 +6,7 @@ from database.db_connect import get_connection
 def load_customers(df,conn):
     customer_df = df[['customer_id', 'customer_name', 'segment', 'region']].drop_duplicates()
     customer_df.to_sql('customer', conn, if_exists='replace', index=False)
-    print(f"Loaded {len(customer)}customers")
+    print(f"Loaded {len(customer_df)}customers")
 
 def load_products(df,conn):
     product_df = df[['product_id', 'product_name', 'category', 'sub_category']].drop_duplicates()
