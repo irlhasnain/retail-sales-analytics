100784cb7bd46d91549ea4f4a998744a74c25650
irlhasnain <hasnainkh3011@gmail.com>
2026-07-19 13:44:17 +0530

    Connect dashboard to database and analytics functions

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import sys
+sys.path.append('.')
+import pandas as pd
+from database.db_connect import get_connection
+from sql_queries.analytics import monthly_revenue, top_product, customer_segmentation
+
+st.set_page_config(page_title="Retail Sales Anlytics",layout="wide")
 
 st.title("📊 Retail Sales Analytics Dashboard")
 st.write("Welcome to the dashboard!")
